01376fa20ab4c4d9301e90e554b6e2958326ea73
wanderson-aldo <wanderson.aldo@gmail.com>
2024-05-24 10:59:42 -0300

    network

---
+++ src/components/GetIpAddress/index.js
@@ -0,0 +1,40 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import * as Network from "expo-network";
+import { useState, useEffect } from "react";
+
+export default function App() {
+  const [ipAddress, setIpAddress] = useState(undefined);
+  const [networkState, setNetworkState] = useState(undefined);
+
+  useEffect(() => {
+    const getIpAddress = async () => {
+      const ip = await Network.getIpAddressAsync();
+      setIpAddress(ip);
+    };
+    getIpAddress();
+
+    const getNetworkState = async () => {
+      const state = await Network.getNetworkStateAsync();
+      setNetworkState(JSON.stringify(state));
+    };
+    getNetworkState();
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <Text>{ipAddress}</Text>
+      <Text>{networkState}</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
